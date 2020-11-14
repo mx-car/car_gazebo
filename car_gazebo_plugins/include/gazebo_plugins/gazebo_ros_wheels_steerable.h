392b3ce613b1d25551e958d2927943070f5d4f62
Markus Bader <markus.bader@mx-robotics.com>
2020-11-14 18:27:08 +0100

    plugin added

--- car_gazebo_plugins/include/gazebo_plugins/gazebo_ros_wheels_steerable.h
+++ car_gazebo_plugins/include/gazebo_plugins/gazebo_ros_wheels_steerable.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, Daniel Hewlett, Antons Rebguns
+ * Copyright (c) 2020, Markus Bader
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -27,19 +27,12 @@
  **/
 
 /*
- * \file  gazebo_ros_diff_drive.h
- *
- * \brief A differential drive plugin for gazebo. Based on the diffdrive plugin
- * developed for the erratic robot (see copyright notice above). The original
- * plugin can be found in the ROS package gazebo_erratic_plugins.
- *
- * \author  Piyush Khandelwal (piyushk@gmail.com)
- *
- * $ Id: 06/21/2013 11:23:40 AM piyushk $
+ * @file  gazebo_ros_wheel_steerable.h
+ * @brief A driver to control one or more steerable wheels
  */
 
-#ifndef ACKERMANN_PLUGIN_HH
-#define ACKERMANN_PLUGIN_HH
+#ifndef WHEEL_STEERABLE_PLUGIN_HH
+#define WHEEL_STEERABLE_PLUGIN_HH
 
 #include <map>
 
@@ -70,16 +63,17 @@ namespace gazebo {
   class Joint;
   class Entity;
 
-  class GazeboRosAckermannDrive : public ModelPlugin {
+  class GazeboRosWheelsSteerable : public ModelPlugin {
 
-    enum OdomSource
-    {
-        ENCODER = 0,
-        WORLD = 1,
+    enum Wheel {
+        REAR_RIGHT = 0,
+        REAR_LEFT = 1,
+        FRONT_RIGHT = 2,
+        FRONT_LEFT = 3,
     };
     public:
-      GazeboRosAckermannDrive();
-      ~GazeboRosAckermannDrive();
+      GazeboRosWheelsSteerable();
+      ~GazeboRosWheelsSteerable();
       void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
       void Reset();
 
@@ -88,68 +82,40 @@ namespace gazebo {
       virtual void FiniChild();
 
     private:
-      void publishOdometry(double step_time);
-      void getWheelVelocities();
-      void publishWheelTF(); /// publishes the wheel tf's
-      void publishWheelJointState();
-      void UpdateOdometryEncoder();
 
 
       GazeboRosPtr gazebo_ros_;
       physics::ModelPtr parent;
       event::ConnectionPtr update_connection_;
 
-      double wheel_separation_;
-      double wheel_diameter_;
-      double wheel_torque;
-      double wheel_speed_[2];
-	  double wheel_accel;
-      double wheel_speed_instr_[2];
 
-      std::vector<physics::JointPtr> joints_;
+      std::vector<physics::JointPtr> joints_rotation_;
+      std::vector<physics::JointPtr> joints_steering_;
 
       // ROS STUFF
-      ros::Publisher odometry_publisher_;
       ros::Subscriber cmd_vel_subscriber_;
-      boost::shared_ptr<tf::TransformBroadcaster> transform_broadcaster_;
-      sensor_msgs::JointState joint_state_;
-      ros::Publisher joint_state_publisher_;
-      nav_msgs::Odometry odom_;
-      std::string tf_prefix_;
 
       boost::mutex lock;
+      bool alive_;
 
-      std::string robot_namespace_;
-      std::string command_topic_;
-      std::string odometry_topic_;
-      std::string odometry_frame_;
-      std::string robot_base_frame_;
-      bool publish_tf_;
+      std::string topic_cmd_;
+      std::string topic_odom_;
+      std::string frame_odom_;
+      std::string frame_base_;
+      double update_rate_controller_;
+      double wheel_torque;
+      
       // Custom Callback Queue
       ros::CallbackQueue queue_;
       boost::thread callback_queue_thread_;
       void QueueThread();
 
       // DiffDrive stuff
-      void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& cmd_msg);
-
-      double x_;
-      double rot_;
-      bool alive_;
+      void callbackTopicCMD(const geometry_msgs::Twist::ConstPtr& cmd_msg);
 
       // Update Rate
-      double update_rate_;
-      double update_period_;
       common::Time last_update_time_;
-
-      OdomSource odom_source_;
-      geometry_msgs::Pose2D pose_encoder_;
-      common::Time last_odom_update_;
-
-    // Flags
-    bool publishWheelTF_;
-    bool publishOdomTF_;
-    bool publishWheelJointState_;
+      
 
   };
 
---
+++ car_gazebo_plugins/src/gazebo_ros_wheels_steerable.cpp
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2020, Markus Bader
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *      * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *      * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *      * Neither the name of the <organization> nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY Antons Rebguns <email> ''AS IS'' AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL Antons Rebguns <email> BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **/
+
+#include <algorithm>
+#include <assert.h>
+
+#include <gazebo_plugins/gazebo_ros_wheels_steerable.h>
+
+#ifdef ENABLE_PROFILER
+#include <ignition/common/Profiler.hh>
+#endif
+
+#include <ignition/math/Angle.hh>
+#include <ignition/math/Pose3.hh>
+#include <ignition/math/Quaternion.hh>
+#include <ignition/math/Vector3.hh>
+#include <sdf/sdf.hh>
+
+#include <ros/ros.h>
+
+namespace gazebo
+{
+
+
+GazeboRosWheelsSteerable::GazeboRosWheelsSteerable() {}
+
+// Destructor
+GazeboRosWheelsSteerable::~GazeboRosWheelsSteerable()
+{
+    FiniChild();
+}
+
+// Load the controller
+void GazeboRosWheelsSteerable::Load ( physics::ModelPtr _parent, sdf::ElementPtr _sdf )
+{
+
+    this->parent = _parent;
+    gazebo_ros_ = GazeboRosPtr ( new GazeboRos ( _parent, _sdf, "WheelsSteerable" ) );
+    // Make sure the ROS node for Gazebo has already been initialized
+    gazebo_ros_->isInitialized();
+
+    gazebo_ros_->getParameter<std::string> ( topic_cmd_,                "topicCMD",  "cmd_vel" );
+    gazebo_ros_->getParameter<std::string> ( topic_odom_,               "topicOdom", "odom" );
+    gazebo_ros_->getParameter<std::string> ( frame_odom_,               "frameOdom", "odom" );
+    gazebo_ros_->getParameter<std::string> ( frame_base_,               "frameBase", "base_link" );
+    gazebo_ros_->getParameter<double>      ( update_rate_controller_,   "updateRateController", 100.0 );
+    
+    gazebo_ros_->getParameter<double> ( wheel_torque, "wheelTorque", 5.0 );
+
+    // ROS: Subscribe to the velocity command topic (usually "cmd_vel")
+    ROS_INFO_NAMED("diff_drive", "%s: Try to subscribe to %s", gazebo_ros_->info(), topic_cmd_.c_str());
+
+    joints_rotation_[REAR_LEFT ] = gazebo_ros_->getJoint ( parent, "wheel_axis_rear_left_joint",  "rear_left_joint"  );
+    joints_rotation_[REAR_RIGHT] = gazebo_ros_->getJoint ( parent, "wheel_axis_rear_right_joint", "rear_right_joint" );
+    //joints_rotation_[REAR_LEFT ]->SetParam ( "fmax", 0, wheel_torque );
+    //joints_rotation_[REAR_RIGHT]->SetParam ( "fmax", 0, wheel_torque );
+    joints_rotation_[REAR_LEFT ]->SetParam ( "vel", 0, 1 );
+    joints_rotation_[REAR_RIGHT]->SetParam ( "vel", 0, 1 );
+
+
+    ros::SubscribeOptions so =
+        ros::SubscribeOptions::create<geometry_msgs::Twist>(topic_cmd_, 1,
+                boost::bind(&GazeboRosWheelsSteerable::callbackTopicCMD, this, _1),
+                ros::VoidPtr(), &queue_);
+
+    cmd_vel_subscriber_ = gazebo_ros_->node()->subscribe(so);
+    ROS_INFO_NAMED("WheelsSteerable", "%s: Subscribe to %s", gazebo_ros_->info(), topic_cmd_.c_str());
+
+
+    // start custom queue for diff drive
+    this->callback_queue_thread_ =
+        boost::thread ( boost::bind ( &GazeboRosWheelsSteerable::QueueThread, this ) );
+
+    // listen to the update event (broadcast every simulation iteration)
+    this->update_connection_ =
+        event::Events::ConnectWorldUpdateBegin ( boost::bind ( &GazeboRosWheelsSteerable::UpdateChild, this ) );
+
+    alive_ = true;
+}
+
+void GazeboRosWheelsSteerable::Reset()
+{
+    last_update_time_ = parent->GetWorld()->SimTime();
+}
+
+
+// Update the controller
+void GazeboRosWheelsSteerable::UpdateChild()
+{
+#ifdef ENABLE_PROFILER
+    IGN_PROFILE("GazeboRosWheelsSteerable::UpdateChild");
+    IGN_PROFILE_BEGIN("update");
+#endif
+
+#ifdef ENABLE_PROFILER
+    IGN_PROFILE_END();
+#endif
+}
+
+// Finalize the controller
+void GazeboRosWheelsSteerable::FiniChild()
+{
+    alive_ = false;
+    queue_.clear();
+    queue_.disable();
+    gazebo_ros_->node()->shutdown();
+    callback_queue_thread_.join();
+}
+void GazeboRosWheelsSteerable::QueueThread()
+{
+    static const double timeout = 0.01;
+
+    while ( alive_ && gazebo_ros_->node()->ok() ) {
+        queue_.callAvailable ( ros::WallDuration ( timeout ) );
+    }
+}
+
+void GazeboRosWheelsSteerable::callbackTopicCMD ( const geometry_msgs::Twist::ConstPtr& cmd_msg )
+{
+}
+
+GZ_REGISTER_MODEL_PLUGIN ( GazeboRosWheelsSteerable )
+}
